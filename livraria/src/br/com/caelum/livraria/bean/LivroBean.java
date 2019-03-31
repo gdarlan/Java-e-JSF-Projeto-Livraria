4d70697e6df927a26e00513eceb9d65a96904fe7
gdarlan <gdarlan@gdarlan-VirtualBox>
2019-03-31 13:01:52 -0300

    Aula 05 - 01

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -33,18 +37,23 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			/*
+			 * // throw new RuntimeException("Livro deve ter pelo menos um Autor."); ////
+			 * FacesContext.getCurrentInstance().addMessage("autor", //// new
+			 * FacesMessage("Livro deve ter pelo menos um Autor."));
+			 */ 
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
+		
+		}else {
+			new DAO<Livro>(Livro.class).adiciona(this.livro);
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
 	}
 
-	
-	public List<Livro> getLivros(){
+	public List<Livro> getLivros() {
 		return new DAO<Livro>(Livro.class).listaTodos();
 	}
-	
-	
+
 	public List<Autor> getAutoresDoLivro() {
 		return this.livro.getAutores();
 	}
@@ -55,6 +64,13 @@ public class LivroBean {
 
 	public void setAutorId(Integer autorId) {
 		this.autorId = autorId;
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com !"));
+		}
 	}
 
 }
