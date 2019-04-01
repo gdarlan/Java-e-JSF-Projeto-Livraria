54d1ead64694d3de7d801931038b161dcb111296
gdarlan <gdarlan@gdarlan-VirtualBox>
2019-03-31 21:38:31 -0300

    Capitulo 9 completo

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,7 +1,11 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -17,14 +21,30 @@ public class AutorBean {
 	}
 
 	public String gravar() {
-		System.out.println("Gravando autor " + this.autor.getNome());
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
-		
-		return "livro?faces-redirect=true";
+		if (this.autor.getId() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+			return "livro?faces-redirect=true";
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(autor);
 
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Alteração efetuada com sucesso!"));
+			limpar();
+			return null;
+		}
 	}
 
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	public void remover(Autor autor) {
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+
+	public void carregar(Autor autor) {
+		this.autor = autor;
+	}
 
 	public void limpar() {
 		this.autor = new Autor();
