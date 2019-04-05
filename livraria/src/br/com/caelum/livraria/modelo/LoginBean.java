ebd246b6e384782d191ea8685bf4bbc16c591797
gdarlan <gdarlan@gdarlan-VirtualBox>
2019-04-04 22:43:40 -0300

    tela login pronto

--- livraria/src/br/com/caelum/livraria/modelo/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/modelo/LoginBean.java
@@ -13,30 +13,23 @@ public class LoginBean {
 
 	private Usuario usuario = new Usuario();
 
-	public String efetuaLogin() {
-		System.out.println(
-				"Fazendo login do usuário " + this.getUsuario().getEmail() + " " + this.getUsuario().getSenha());
-
-		boolean existe = new UsuarioDao().existe(this.getUsuario());
-
-		FacesContext context = FacesContext.getCurrentInstance();
-		if (existe) {
-			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
-			return "livro?faces-redirect=true";
-		} else {
-			context.addMessage(null, new FacesMessage("Usuário não encontrado"));
-			context.getExternalContext().getFlash();
-			return "login?faces-redirect=true";
-		}
-
-	}
-
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
+	public String efetuaLogin() {
+		System.out.println("fazendo login do usuario " + this.usuario.getEmail());
+
+		FacesContext context = FacesContext.getCurrentInstance();
+		boolean existe = new UsuarioDao().existe(this.usuario);
+		if (existe) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
+		}
+
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario não encontrado", null));
+		return "login?faces-redirect=true";
 	}
 
 	public String deslogar() {
